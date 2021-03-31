18004e28f33bd3a7da7cc483dd9fe6b224df45e5
learn-to-hack <abhaysinghs772@gmail.com>
2021-03-31 23:10:45 +0530

    made some changes

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -4,7 +4,7 @@ using namespace std;
 char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
 
 char current_Marker;
-
+int current_Player;
 
 void draw_Board()
 {
@@ -24,29 +24,66 @@ void draw_Board()
 
 void place_Marker(int slot)
 {
-    int row = slot/3 ;
-    int col = (slot % 3) - 1;
+    int row = slot / 3;
+    int col = (slot % 3) - 1; // for multiples of 3 i.e; 3,,6,9
     if (slot % 3 == 0)
     {
         row = row - 1;
         col = 2;
     }
-    // else
-    // {
-    //     col = (slot % 3) - 1;
-    // }   
 
-    cout << row << ',' << col;
+    board[row][col] = current_Marker;
+}
+
+int winner()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        // rows
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
+            return current_Player;
+
+        // colomns
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
+            return current_Player;
+    }
+
+    // diagonal
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+        return current_Player;
+
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+        return current_Player;
+
+    return 0;
+}
+
+void game()
+{
+    cout << "Player one, choose your marker : ";
+    char marker_P1;
+    cin >> marker_P1;
+
+    current_Player = 1;
+    current_Marker = marker_P1;
+
+    draw_Board();
+    for (int i = 0; i < 9; i++)
+    {
+        cout << "It's player " << current_Player << "'s turn. Enter your slot : ";
+        int slot;
+        cin >> slot;
+        place_Marker(slot);
+        draw_Board();
+    }
+    
 }
 
 
 int main()
 {
-    // draw_Board();
-    place_Marker(9);
+    game();
     return 0;
 }
 
-// 12:28
-
-// testing
+// 22
