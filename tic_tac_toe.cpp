427f05c81aaebfc994b52d7710b52ad09d557dd0
learn-to-hack <abhaysinghs772@gmail.com>
2021-04-02 11:47:04 +0530

    Working on psudo AI

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+
+
 using namespace std;
 
 char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
@@ -22,7 +24,7 @@ void draw_Board()
     }
 }
 
-void place_Marker(int slot)
+bool place_Marker(int slot)
 {
     int row = slot / 3;
     int col = (slot % 3) - 1; // for multiples of 3 i.e; 3,,6,9
@@ -32,7 +34,15 @@ void place_Marker(int slot)
         col = 2;
     }
 
-    board[row][col] = current_Marker;
+    if (board[row][col] != 'X' && board[row][col] != 'O')
+    {
+        board[row][col] = current_Marker;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 int winner()
@@ -57,6 +67,25 @@ int winner()
 
     return 0;
 }
+void swap_player_and_marker()
+{
+    if (current_Marker == 'X')
+    {
+        current_Marker = 'O';
+    }
+    else
+    {
+        current_Marker = 'X';
+    }
+    if (current_Player == 1)
+    {
+        current_Player = 2;
+    }
+    else
+    {
+        current_Player = 1;
+    }
+}
 
 void game()
 {
@@ -67,18 +96,50 @@ void game()
     current_Player = 1;
     current_Marker = marker_P1;
 
+    int player_Won;
+
     draw_Board();
     for (int i = 0; i < 9; i++)
     {
         cout << "It's player " << current_Player << "'s turn. Enter your slot : ";
         int slot;
         cin >> slot;
-        place_Marker(slot);
-        draw_Board();
-    }
-    
-}
 
+        if (slot < 1 || slot > 9)
+        {
+            cout << "the slot is occupied! Try another slot!" << endl;
+            i--;
+            continue;
+        }
+
+        if (!place_Marker(slot))
+        {
+            cout << "the slot is occupied! Try another slot!"<<endl;
+            i--;
+            continue;
+        }
+
+        draw_Board();
+        player_Won = winner();
+
+        if (player_Won == 1)
+        {
+            cout << "player one won! Congratulations!";
+            break;
+        }
+        if (player_Won == 2)
+        {
+            cout << "player two won! Congratulations!";
+            break;
+        }
+
+        swap_player_and_marker();
+    }
+    if (player_Won == 0)
+    {
+        cout << "thats a tie game!";
+    }
+}
 
 int main()
 {
@@ -86,4 +147,4 @@ int main()
     return 0;
 }
 
-// 22
+// 34:35
